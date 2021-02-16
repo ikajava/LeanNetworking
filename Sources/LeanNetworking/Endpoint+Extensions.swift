36ea5ac62f0ac220b2daf5dfd35ce765e3d71043
Shalva Avanashvili <shalva@onoffapp.com>
2021-02-16 15:01:05 +0200

    AL-3271 add custom network error

--- Sources/LeanNetworking/Endpoint+Extensions.swift
+++ Sources/LeanNetworking/Endpoint+Extensions.swift
@@ -1,84 +1,39 @@
-//
-//  Endpoint+Extensions.swift
-//  LeanNetworking
-//
-//  Created by Oleksandr Glagoliev on 13.07.2020.
-//  Copyright © 2020 Oleksandr Glagoliev. All rights reserved.
-//
 
 import Foundation
 import Combine
 
-
-func authoriseUser() -> AnyPublisher<Bool, Never> {
-    // REFRESH TOKEN
-    #warning("Incorrect implementation")
-    return Just(false)
-        .eraseToAnyPublisher()
-}
-
-
 public extension Endpoint {
-    func asPublisher() -> AnyPublisher<Response, Error> {
-        URLSession
-            .shared
-            .dataTaskPublisher(for: request)
-            .tryMap { result in
-                guard let response = result.response as? HTTPURLResponse,
-                    (200...299).contains(response.statusCode)
-                    else {
-                        throw try self.decoder.decode(APIError.self, from: result.data.valid)
-                }
-                
-                Logger.log(loggingOptions: self.loggingOptions,
-                           level: .info,
-                           response: result.response,
-                           data: result.data.valid)
-                
-                return try self.decoder.decode(Response.self, from: result.data.valid)
-        }
-        .tryCatch({ error -> AnyPublisher<Response, Error> in
-            guard
-                let apiError = error as? APIError,
-                apiError.statusCode == 401
-                else {
-                    Logger.trace(level: .error, params: [String(describing: error)])
-                    throw error
-            }
-            return authoriseUser()
-                .tryMap { [weak self] success -> AnyPublisher<Response, Error> in
-                    guard let self = self else {
-                        Logger.trace(level: .error, params: [String(describing: error)])
-                        throw error
-                    }
-                    guard success else {
-                        Logger.trace(level: .error, params: [String(describing: error)])
-                        throw error
-                    }
-                    return self.asPublisher()
-            }
-            .switchToLatest()
-            .eraseToAnyPublisher()
-            }
-        ).eraseToAnyPublisher()
-    }
     
-    func asFuture() -> Future<Response, Error> {
+    func asFuture() -> Future<Response, NetworkingError> {
         Logger.log(loggingOptions: loggingOptions, level: .info, request: request)
         
-        return Future<Response, Error> { promise in
+        return Future<Response, NetworkingError> { promise in
             let task = URLSession.shared.dataTask(with: self.request) { data, response, error in
                 
                 guard error == nil else {
-                    promise(.failure(error!))
+                    promise(.failure( NetworkingError.regular(error!) ))
                     Logger.trace(level: .error, params: [String(describing: error)])
                     return
                 }
+                
                 do {
                     Logger.log(loggingOptions: self.loggingOptions, level: .info, response: response, data: data!.valid)
                     promise(.success(try self.decoder.decode(Response.self, from: data!.valid)))
                 } catch let error {
-                    promise(.failure(error))
+                    
+                    do {
+                        // try to decode response in status error
+                        let networkError = try self.decoder.decode(NetworkingError.self, from: data!.valid)
+                        if case .status = networkError {
+                            promise(.failure(networkError))
+                        }
+                        
+                        return
+                    } catch {
+                        // DO NOTHING
+                    }
+                    
+                    promise(.failure( NetworkingError.regular(error) ))
                     Logger.traceDecodingError(error: error)
                 }
             }
@@ -86,18 +41,18 @@ public extension Endpoint {
         }
     }
     
-    func call(completion: @escaping (Result<Response, Error>) -> Void) {
+    func call(completion: @escaping (Result<Response, NetworkingError>) -> Void) {
         let task = URLSession
             .shared
             .dataTask(with: request) { data, response, error in
                 guard error == nil else {
-                    completion(.failure(error!))
+                    completion(.failure(  NetworkingError.regular(error!) ))
                     return
                 }
                 do {
                     completion(.success(try self.decoder.decode(Response.self, from: data!.valid)))
                 } catch let error {
-                    completion(.failure(error))
+                    completion(.failure( NetworkingError.regular(error) ))
                 }
         }
         task.resume()
---
+++ Sources/LeanNetworking/NetworkingError.swift
@@ -0,0 +1,38 @@
+
+import Foundation
+
+
+public enum NetworkingError: Error, Codable {
+    case status(String)
+    case regular(Error)
+}
+
+extension NetworkingError {
+
+    private enum CodingKeys: String, CodingKey {
+        case status
+    }
+
+    enum NetworkingErrorCodingError: Error {
+        case decoding(String)
+    }
+
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        if let value = try? values.decode(String.self, forKey: .status) {
+            self = .status(value)
+            return
+        }
+        
+        throw NetworkingErrorCodingError.decoding("Whoops! \(dump(values))")
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case let .status(errorStatus):
+            try container.encode(errorStatus, forKey: .status)
+        default: break
+        }
+    }
+}
