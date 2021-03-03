0a489cd7299736d426199ef9693a452b0b458234
Oleksandr Glagoliev <oleksandr@onoffapp.com>
2021-03-03 17:33:02 +0200

    Pinning to staging

--- Sources/LeanNetworking/Pin.swift
+++ Sources/LeanNetworking/Pin.swift
@@ -3,72 +3,38 @@ import CommonCrypto
 
 class Pin : NSObject, URLSessionDelegate {
     
-    private static let rsa2048Asn1Header:[UInt8] = [
+    private let rsa2048Asn1Header:[UInt8] = [
         0x30, 0x82, 0x01, 0x22, 0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86,
         0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00, 0x03, 0x82, 0x01, 0x0f, 0x00
-    ];
+    ]
     
-    private static let pubKey = ["KEMk92zeeRAUvSFH9wiY3T9oaTGOjfsXG1z/z+vooBo="];
-    private static let full = ["47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU="];
+    private let pubKey = "KEMk92zeeRAUvSFH9wiY3T9oaTGOjfsXG1z/z+vooBo="
     
-    func urlSession(_ session: URLSession,
-                    didReceive challenge: URLAuthenticationChallenge,
-                    completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+    
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Swift.Void) {
         
-        guard let serverTrust = challenge.protectionSpace.serverTrust else {
-            completionHandler(.cancelAuthenticationChallenge, nil);
-            return;
-        }
-        
-        // Set SSL policies for domain name check
-        let policies = NSMutableArray();
-        policies.add(SecPolicyCreateSSL(true, (challenge.protectionSpace.host as CFString)));
-        SecTrustSetPolicies(serverTrust, policies);
-        
-        var isServerTrusted = SecTrustEvaluateWithError(serverTrust, nil);
-        
-        if(isServerTrusted && challenge.protectionSpace.host == "staging-723489234y.albums.app") {
-            let certificate = SecTrustGetCertificateAtIndex(serverTrust, 0);
-            //Compare public key
-            if #available(iOS 10.0, *) {
-                let policy = SecPolicyCreateBasicX509();
-                let cfCertificates = [certificate] as CFArray;
-                
-                var trust: SecTrust?
-                SecTrustCreateWithCertificates(cfCertificates, policy, &trust);
-                
-                guard trust != nil, let pubKey = SecTrustCopyPublicKey(trust!) else {
-                    completionHandler(.cancelAuthenticationChallenge, nil);
-                    return;
-                }
-                
-                var error:Unmanaged<CFError>?
-                if let pubKeyData = SecKeyCopyExternalRepresentation(pubKey, &error) {
-                    var keyWithHeader = Data(SessionDelegate.rsa2048Asn1Header);
-                    keyWithHeader.append(pubKeyData as Data)
-                    let sha256Key = sha256(keyWithHeader)
-                    if(!SessionDelegate.pubKey.contains(sha256Key)) {
-                        isServerTrusted = false
+        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+            if let serverTrust = challenge.protectionSpace.serverTrust {
+                if SecTrustEvaluateWithError(serverTrust, nil) {
+                    
+                    if let serverCertificate = SecTrustGetCertificateAtIndex(serverTrust, 0) {
+                        // Public key pinning
+                        let serverPublicKey = SecCertificateCopyKey(serverCertificate)
+                        let serverPublicKeyData: NSData = SecKeyCopyExternalRepresentation(serverPublicKey!, nil )!
+                        let keyHash = sha256(serverPublicKeyData as Data)
+                        if keyHash == pubKey {
+                            // Success! This is our server
+                            completionHandler(.useCredential, URLCredential(trust:serverTrust))
+                            return
+                        }
+                        
                     }
-                } else {
-                    isServerTrusted = false
-                }
-            } else { //Compare full certificate
-                let remoteCertificateData = SecCertificateCopyData(certificate!) as Data
-                let sha256Data = sha256(remoteCertificateData);
-                if(!SessionDelegate.full.contains(sha256Data)) {
-                    isServerTrusted = false
                 }
             }
         }
         
-        if(isServerTrusted) {
-            let credential = URLCredential(trust: serverTrust);
-            completionHandler(.useCredential, credential);
-        } else {
-            completionHandler(.cancelAuthenticationChallenge, nil);
-        }
-        
+        // Pinning failed
+        completionHandler(.cancelAuthenticationChallenge, nil)
     }
     
     func sha256(_ data : Data) -> String {
