7e2059d68067c17b68dcf8e6bc06a3bc33173e50
Oleksandr Glagoliev <oleksandr@onoffapp.com>
2021-03-03 18:01:58 +0200

    Revert

--- Sources/LeanNetworking/Pin.swift
+++ Sources/LeanNetworking/Pin.swift
@@ -1,7 +1,7 @@
 import Foundation
 import CommonCrypto
 
-class Pin: NSObject, URLSessionDelegate {
+class Pin : NSObject, URLSessionDelegate {
     
     private let rsa2048Asn1Header:[UInt8] = [
         0x30, 0x82, 0x01, 0x22, 0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86,
@@ -10,31 +10,56 @@ class Pin: NSObject, URLSessionDelegate {
     
     private let pubKey = "KEMk92zeeRAUvSFH9wiY3T9oaTGOjfsXG1z/z+vooBo="
     
-    
-    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Swift.Void) {
+    func urlSession(
+        _ session: URLSession,
+        didReceive challenge: URLAuthenticationChallenge,
+        completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void
+    ) {
         
-        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
-            if let serverTrust = challenge.protectionSpace.serverTrust {
-                if SecTrustEvaluateWithError(serverTrust, nil) {
-                    
-                    if let serverCertificate = SecTrustGetCertificateAtIndex(serverTrust, 0) {
-                        // Public key pinning
-                        let serverPublicKey = SecCertificateCopyKey(serverCertificate)
-                        let serverPublicKeyData: NSData = SecKeyCopyExternalRepresentation(serverPublicKey!, nil )!
-                        let keyHash = sha256(serverPublicKeyData as Data)
-                        if keyHash == pubKey {
-                            // Success! This is our server
-                            completionHandler(.useCredential, URLCredential(trust: serverTrust))
-                            return
-                        }
-                        
-                    }
-                }
+        guard let serverTrust = challenge.protectionSpace.serverTrust else {
+            completionHandler(.cancelAuthenticationChallenge, nil);
+            return;
+        }
+        
+        // Set SSL policies for domain name check
+        let policies = NSMutableArray()
+        policies.add(SecPolicyCreateSSL(true, (challenge.protectionSpace.host as CFString)))
+        SecTrustSetPolicies(serverTrust, policies)
+        
+        var isServerTrusted = SecTrustEvaluateWithError(serverTrust, nil);
+        
+        if(isServerTrusted && challenge.protectionSpace.host == "staging-723489234y.albums.app") {
+            let certificate = SecTrustGetCertificateAtIndex(serverTrust, 0)
+            
+            let policy = SecPolicyCreateBasicX509()
+            let cfCertificates = [certificate] as CFArray
+            
+            var trust: SecTrust?
+            SecTrustCreateWithCertificates(cfCertificates, policy, &trust)
+            
+            guard trust != nil, let pubKey = SecTrustCopyPublicKey(trust!) else {
+                completionHandler(.cancelAuthenticationChallenge, nil)
+                return
+            }
+            
+            var error:Unmanaged<CFError>?
+            if let pubKeyData = SecKeyCopyExternalRepresentation(pubKey, &error) {
+                var keyWithHeader = Data(rsa2048Asn1Header)
+                keyWithHeader.append(pubKeyData as Data)
+                let sha256Key = sha256(keyWithHeader)
+                isServerTrusted = pubKey as! String == sha256Key
+            } else {
+                isServerTrusted = false
             }
         }
         
-        // Pinning failed
-        completionHandler(.cancelAuthenticationChallenge, nil)
+        if isServerTrusted {
+            let credential = URLCredential(trust: serverTrust)
+            completionHandler(.useCredential, credential)
+        } else {
+            completionHandler(.cancelAuthenticationChallenge, nil)
+        }
+        
     }
     
     func sha256(_ data : Data) -> String {
