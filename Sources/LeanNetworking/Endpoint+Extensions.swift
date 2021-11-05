b5b48364efe1d8b77a9f109a74c4ef04b519dcaf
Shalva Avanashvili <avanashvili5@gmail.com>
2021-11-05 12:07:04 +0200

    Improve logic error handling

--- Sources/LeanNetworking/Endpoint+Extensions.swift
+++ Sources/LeanNetworking/Endpoint+Extensions.swift
@@ -32,6 +32,22 @@ public extension Endpoint {
                     return
                 }
                 
+                
+                if statusCode == .logicException {
+                    do {
+                        // try to decode response in status error
+                        let networkError = try self.decoder.decode(NetworkingError.self, from: data!.valid)
+                        if case .status = networkError {
+                            promise(.failure(networkError))
+                        }
+                        
+                        return
+                    } catch {
+                        promise(.failure( NetworkingError.regular(error, statusCode) ))
+                        Logger.traceDecodingError(error: error)
+                    }
+                }
+                
                 guard error == nil else {
                     promise(.failure( NetworkingError.regular(error, statusCode) ))
                     Logger.trace(level: .error, params: [String(describing: error)])
@@ -42,19 +58,6 @@ public extension Endpoint {
                     Logger.log(loggingOptions: self.loggingOptions, level: .info, response: response, data: data!.valid)
                     promise(.success(try self.decoder.decode(Response.self, from: data!.valid)))
                 } catch let error {
-                    
-                    do {
-                        // try to decode response in status error
-                        let networkError = try self.decoder.decode(NetworkingError.self, from: data!.valid)
-                        if case .status = networkError {
-                            promise(.failure(networkError))
-                        }
-                        
-                        return
-                    } catch {
-                        // DO NOTHING
-                    }
-                    
                     promise(.failure( NetworkingError.regular(error, statusCode) ))
                     Logger.traceDecodingError(error: error)
                 }
